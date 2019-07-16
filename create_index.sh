9d5ae1bdaa0aea58a4ff5dcb1a79f71eebf18030
kxxmfei <644694932@qq.com>
2019-07-16 17:40:54 +0800

    sh修改

--- create_index.sh
+++ create_index.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/php7.3/bin/php ./xs_sdk/util/Indexer.php --rebuild --source=mysql://root:q19900516@192.168.1.105/sh_article --sql="SELECT `id`,`title`,`desc`,`content` FROM sh_article" --project=test
+/usr/local/php7.3/bin/php ./xs_sdk/util/Indexer.php --rebuild --source=mysql://root:q19900516@192.168.1.105/sh_article --sql="SELECT id,title,desc,content FROM sh_article" --project=test
