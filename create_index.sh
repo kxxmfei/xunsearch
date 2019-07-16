a9db42c05c07e851ba8d2a66611a9e5422ea88d2
kxxmfei <644694932@qq.com>
2019-07-16 19:13:11 +0800

    创建索引字段类型修改

--- create_index.sh
+++ create_index.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/php7.3/bin/php ./xs_sdk/util/Indexer.php --rebuild --source=mysql://root:q19900516@192.168.1.105/test_db --sql="SELECT id,title,desc,content FROM sh_article" --project=test
+/usr/local/php7.3/bin/php ./xs_sdk/util/Indexer.php --rebuild --source=mysql://root:123456@192.168.1.105/test_db/sh_article --project=test
