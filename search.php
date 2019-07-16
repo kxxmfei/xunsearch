b1ff0636a8233b0c5171e7e3d1445842caedb911
kxxmfei <644694932@qq.com>
2019-07-16 19:48:53 +0800

    添加搜索排序和匹配度

--- search.php
+++ search.php
@@ -5,13 +5,14 @@ if($keyword){
 	$xs = new xs('test');  // test  为项目名称，配置文件是：$sdk/app/test.ini
 	$search = $xs->search;
 	$search->setLimit(20);
+	$search->setSort('id');
 	$docs = $search->setQuery($keyword)->search();
 	echo "<ul>";
 	foreach($docs as $key=>$doc){
 		$id = $doc->id;
 		$desc = $doc->desc;
 		$title = $search->highlight($doc->title);
-		echo "<li><b>{$id}:{$title}</b><br/>描述:<br/>{$desc}</li>";
+		echo "<li><b>{$id}:{$title}</b>（匹配度：".$doc->percent()."）<br/>描述:<br/>{$desc}</li>";
 	}
 	echo "</ul>";
 }
